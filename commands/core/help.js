475cf065f07316030b1c4a39a174a224917536d0
nForce6791 <nforce6791@proton.me>
2022-04-16 19:49:58 -0400

    edited hack.js / added new help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -10,15 +10,38 @@ module.exports = {
 
     async execute(client, message, args) {
 
-        let helpEmbed = new Discord.MessageEmbed()
-            .setTitle(`Help Panel`)
-            .setAuthor(`nForce RP Help | Prefix: >`)
-            .setColor('#FF0000')
-            .setDescription(`Welcome to the nForce RP help panel! Here you can find all the commands you can use in the server. \n\n **Application Commands:** \n ____>appacceptDEPT <user> <reason>____ \n EG: **>appacceptbcso @nForce#6377 Seems like they know everything such as 10-codes well, has prior knowledge.** \n Those application commands work for staff, SAFR/SAFD, BCSO, LSPD, SAHP + soon to be more. Also works if you do **>acDISCRIMINATOR <user> <reason>**\n The discriminator is what the dept is know as. BCSO is SO, LSPD is PD, so on, so forth. Example: **>acpd @Dyno For no reason** \n\n **>appdenyDEPT <user> <reason>** also works, and so does **>adDISCRIMINATOR <user> <reason>** \n\n **Other commands:** \n ____>ping____ \n Shows bots ping \n\n **Core Commands:** \n ____>help____ \n Displays *this* help message \n\n **Fun Commands:**\n ____>hack <user>____ \n Hacks another user (not real, fake things, promise. not possible through a discord bot) \n ____>8ball____ <question> \n Asks the *magic* 8ball a question \n ____>avatar____ <member> \n displays the mentioned users avatar, if no user is mentioned, it will show your own avatar. ___>meme___ \n Pretty self-explanatory.`)
-            .setFooter(`Requested by ${message.author.tag}`, `${message.author.displayAvatarURL()}`)
-            .setTimestamp()
+        //We have to set a argument for the help command beacuse its going to have a seperate argument.
+        let helpArray = message.content.split(" ");
+        let helpArgs = helpArray.slice(1);
 
-        message.delete();
-        message.channel.send(helpEmbed);
+    //Normal usage of (prefix)help without any args. (Shows all of the commands and you should set the commands yourself)
+        if(!helpArgs[0]) {
+            var embed = new Discord.MessageEmbed()
+                .setAuthor(`Here are the current bot commands:`)
+                .setDescription('```hack | appacceptDEPT | appdenyDEPT | help | 8ball | avatar | meme | ping```')
+                .addFields({ name: 'Prefix', value: '```>```', inline: true})
+                .setColor('#00FFF3')
+            
+            message.channel.send(embed);
+        }
 
+        //Reads the moudle.exports.config (This line of code is on commands folder, each command will read automaticly) by the second argument (the command name) and shows the information of it.
+        if(helpArgs[0]) {
+            let command = helpArgs[0];
+
+            if(client.commands.has(command)) {
+            
+                command = client.commands.get(command);
+                var embed = new Discord.MessageEmbed()
+                    .setAuthor(`${helpArgs} Command`)
+                    .setDescription(`
+                    - **Command's Description** ${command.description || "There is no Description for this command."}
+                    - **Command's Usage:** ${command.utilisation || "No Usage"}
+                    - **Command's Aliases:** ${command.aliases || "No Aliases"}
+                    - **Command's Category:** ${command.category || "No Category"}
+                    `)
+                    .setColor('#2EFF00')
+
+            message.channel.send(embed);
     }}
+}}
